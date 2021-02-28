753a97f31f4f9302814075d74671d9d59e872584
Jose Miguel Ruiz <jose.ruiz.rami@gmail.com>
2021-02-28 10:47:21 -0300

    ✨ Constructor creation, main state definition, bind to events.

---
+++ src/components/add-post-component.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import PostsServices from "../services/PostsServices";
+
+export default class AddPost extends Component {
+    constructor(props) {
+        super(props);
+        this.onChangeTitle = this.onChangeTitle.bind(this);
+        this.onChangeBody = this.onChangeBody.bind(this);
+        this.savePost = this.savePost.bind(this);
+        this.newPost = this.newPost.bind(this);
+
+        this.state = {
+            id: null,
+            title: "",
+            body: "",
+            publiched: false,
+            submitted: false
+        }
+    }
+
+    
+
+    render() {
+        //
+    }
+}
