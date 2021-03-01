8c396af5b134d73635e810c871c2cd8fe4ac7296
Jose Miguel Ruiz <jose.ruiz.rami@gmail.com>
2021-03-01 11:21:46 -0300

    🚧  Importing component paths

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { Switch, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
+import AddPost from "./components/add-post-component";
+import Post from "./components/post-detail-component";
+import PostsList from "./components/posts-list-component";
 
 class App extends Component {
   render() {
